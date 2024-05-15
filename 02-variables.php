c8789567fa5440d3017a91c84c70e30f541619cb
BenjaminPari1478557 <1478557@senati.pe>
2024-05-15 16:54:00 -0500

    otros ejemplos

---
+++ 02-variables.php
@@ -0,0 +1,8 @@
+<?php
+$nombres ="Benjamin";
+$apellido ="Pari";
+$curso ="BACKEND DEVELOPER WEB";
+$semestre ="3er Semestre";
+echo $nombres." ".$apellido." ".$curso." ".$semestre;
+
+?>
---
+++ 03-operaciones.php
@@ -0,0 +1,14 @@
+<?php
+    $numero1=10;
+    $numero2=20;
+
+    $suma=$numero1 +$numero2;
+    $resta=$numero1-$numero2;
+    $multiplicacion =$numero1*$numero2;
+    $division=$numero1/$numero2;
+
+    echo "<h1>La suma es ".$suma."<br>";
+    echo "La resta es ".$resta."<br>";
+    echo "La multiplicacion ".$multiplicacion."<br>";
+    echo "La division ".$division."<br></h1>";
+?>
