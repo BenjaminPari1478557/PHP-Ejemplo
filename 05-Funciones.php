6e9f7ac5f1d670c00cf80d40b4beecec976b2bb2
BenjaminPari1478557 <1478557@senati.pe>
2024-05-15 17:04:40 -0500

    Se agrego la funcion de calcular + - * /

--- 05-Funciones.php
+++ 05-Funciones.php
@@ -1,6 +1,6 @@
 <?php
 function ObtenerSaludo($parametro){
-    return "Hola como estas ".$parametro."<br>";
+    return "Hola como estas ".$parametro."\n";
 
 }
 echo ObtenerSaludo("Benjamin");
@@ -8,5 +8,21 @@ function mostrarDatos($nombres,$apellidos,$curso,$semestre){
     $mensaje="Hola soy ".$nombres." ".$apellidos." y estoy en ".$curso." del ".$semestre;
     return $mensaje;
 }
-echo mostrarDatos("Benjamin","Pari","BACKEND DEVELOPER WEB","3er Semestre")
+echo mostrarDatos("Benjamin","Pari","BACKEND DEVELOPER WEB","3er Semestre")."\n";
+
+function calcular($operador,$numero,$numero2){
+    if($operador=="+"){
+        return $numero+$numero2;
+    }
+    if($operador=="-"){
+        return $numero-$numero2;
+    }
+    if($operador=="*"){
+        return $numero*$numero2;
+    }
+    if($operador=="/"){
+        return $numero/$numero2;
+    }
+}
+echo "El total de la suma es :".calcular("+",10,34)
 ?>
