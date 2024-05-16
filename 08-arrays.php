1104658de482a82af2f8c5e1ce63cbceea33717f
Franklin Ramos Cáceres <franklinramos061@gmail.com>
2024-05-16 15:16:47 -0500

    tarea 08

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,8 +1,22 @@
 <?php
 //caso 1
 $numeros = array(1, 3, 5, 2, 6);
-// for ($i = 0; $i < 0; $i++) {
-//     $numeros[$i] = fgets(STDIN);
-// }
+$frutas = ["fresa","naranja","manzana","mandarina"];
 print_r($numeros);
-echo $numeros [2];
+echo $numeros [2]."\n";
+print_r($frutas);
+//caso 2
+$estudiante = array(
+    "dni            "=>"41558098",
+    "edad           "=>27,
+    "fechanacimiento"=>"2000-01-05",
+    "nombres        "=>"pedro",
+    "apellidos      "=>"paredes acero",
+    "semestre       "=>100.50,
+    "notafinal      "=>11.6
+);
+print_r($estudiante);
+foreach ($estudiante as $key => $value) {
+    echo $key. " => ".$value."\n";
+}
+//caso 3
