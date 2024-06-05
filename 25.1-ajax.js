2f13a354f4f24bfb56c284a533623dab3a93e5bc
BenjaminPari14878557 <1478557@senati.pe>
2024-06-05 17:45:16 -0500

    practica

--- 25.1-ajax.js
+++ 25.1-ajax.js
@@ -10,7 +10,7 @@ function buscarPacientes(){
         data:datos,
         success:function(result){
             //console.log(result);
-            debugger;
+            // debugger;
             console.log(result);
             const pacientes = $.parseJSON(result);
             pacientes.forEach(item => {
@@ -20,17 +20,22 @@ function buscarPacientes(){
     })
     return;
 }
-function agregarFilas(Id,paciente){
+function agregarFilas(id,paciente) {
     const html=
     "<tr>"+
-            "<td>"+paciente.nombres+"</td>"+
-            "<td>"+paciente.edad+"</td>"+
-            "<td>"+paciente.talla_m+"</td>"+
-            "<td>"+paciente.peso_kg+"</td>"+
-            "<td>"+paciente.sintoma_tos+"</td>"+
-            "<td>"+paciente.sintoma_fiebre+"</td>"+
-            "<td>"+paciente.sintoma_disnea+"</td>"+
-            "<td><button type='button'>Editar</></td>"+
-        "</tr>";
-        $(Id + " tr:last").after(html);
+    "<td>"+paciente.nombres+"</td>"+
+    "<td>"+paciente.edad+"</td>"+    
+    "<td>"+paciente.talla_m+"</td>"+
+    "<td>"+paciente.peso_kg+"</td>"+
+    "<td>"+paciente.sintoma_tos+"</td>"+
+    "<td>"+paciente.sintoma_fiebre+"</td>"+
+    "<td>"+paciente.sintoma_disnea+"</td>"+
+    "<td><button type='button' onclick=editar('"+paciente.nombres+"','"+paciente.edad+"');>Editar</button></td>"+
+    "</tr>";
+    $(id+" tr:last").after(html);
+}
+ 
+function editar(nombres,edad) {
+    $('#exampleModal').modal('show');    
+    $("#nombre2").val(nombres);
 }
